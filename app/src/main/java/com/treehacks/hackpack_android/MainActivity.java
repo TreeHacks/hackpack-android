7c85132c8b92855af2ddb8dfd29c36e2fd58e499
Navendra Jha <navendra@asu.edu>
2018-11-22 18:53:57 -0700

    Added UI for MainActivity for Notes Taking Application

--- app/src/main/java/com/treehacks/hackpack_android/MainActivity.java
+++ app/src/main/java/com/treehacks/hackpack_android/MainActivity.java
@@ -1,13 +1,17 @@
 package com.treehacks.hackpack_android;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
